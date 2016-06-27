57b8c9624560c2d5c1cd01aad1bab99a6957ca29
Dmitry Vasilyev <vadv.mkn@gmail.com>
2016-06-27 13:14:31 +0300

    first look

--- mamonsu/lib/first_look_linux.py
+++ mamonsu/lib/first_look_linux.py
@@ -12,7 +12,7 @@ class Shell(object):
     # exit status of timeout code
     TimeoutCode = -1
 
-    def __init__(self, cmd, wait_time=10):
+    def __init__(self, cmd, wait_time=10, sudo=False):
         self.status = 0
         self.cmd = cmd
         self.stdout, self.stderr = None, None
@@ -48,17 +48,30 @@ class SystemInfo(object):
         self.system_release = self._fetch_release()
         self.kernel = self._fetch_kernel()
         self.virtualization = self._fetch_virtualization(self.dmesg)
-        self.raid = self._parsed_raid(self.lspci, self.dmesg)
+        self.parsed_raid = self._parse_raid(self.lspci, self.dmesg)
         self.uptime = self._fetch_uptime()
         self.cpu_arch = self._fetch_cpu_arch()
         self.os_arch = self._fetch_os_arch()
+        self.dmidecode = self._fetch_dmidecode()
         self.system_info = self._fetch_system_info()
         self.mount = self._fetch_mount()
         self.df = self._fetch_df()
         self.cpu_info = self._fetch_cpu_info()
         self.parsed_cpu_info = self._parse_cpu_info(self.cpu_info)
         self.meminfo = self._fetch_meminfo()
-        self.parsed_mem_info = self._parse_meminfo(self.meminfo)
+        self.parsed_meminfo = self._parse_meminfo(self.meminfo)
+
+    _suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+
+    def _humansize(self, nbytes):
+        if nbytes == 0:
+            return '0 B'
+        i = 0
+        while nbytes >= 1024 and i < len(self._suffixes)-1:
+            nbytes /= 1024.
+            i += 1
+        f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+        return '%s %s' % (f, self._suffixes[i])
 
     def _fetch_sysctl(self):
         result = {}
@@ -160,7 +173,7 @@ class SystemInfo(object):
             logging.error(shell.error())
             return ''
 
-    def _parsed_raid(sel, lspci, dmesg):
+    def _parse_raid(sel, lspci, dmesg):
         if lspci != '':
             if re.search(
                 r'RAID bus controller: LSI Logic / Symbios Logic MegaRAID SAS',
@@ -237,13 +250,26 @@ class SystemInfo(object):
             logging.error(shell.error())
         return 'N/A'
 
+    def _fetch_dmidecode(self):
+        sudo = os.getuid() == 0
+        shell = Shell('dmidecode', sudo=sudo)
+        if shell.status == 0:
+            return shell.stdout
+        else:
+            logging.error(shell.error())
+            return ''
+
     def _fetch_system_info(self):
+
         def set_key(result, key, param):
-            shell = Shell('dmidecode -s "{0}"'.format(key), 1)
+            sudo = os.getuid() == 0
+            shell = Shell(
+                'dmidecode -s "{1}"'.format(key), wait_time=1, sudo=sudo)
             if shell.status == 0:
                 result[param] = shell.stdout
             else:
                 logging.error(shell.error())
+
         result = {}
         set_key(result, 'vendor', 'system-manufacturer')
         set_key(result, 'product', 'system-product-name')
@@ -322,4 +348,37 @@ class SystemInfo(object):
         result['model'] = fetch_first(r'model name\s+\:\s+(.*)$', info)
         result['cache'] = fetch_first(r'cache size\s+\:\s+(.*)$', info)
         result['speed'] = fetch_first(r'^cpu MHz\s+\:\s+(\d+\.\d+)$', info)
+        return result
+
+    def _fetch_meminfo(self):
+        if os.path.isfile('/proc/meminfo'):
+            try:
+                with open('/proc/meminfo', 'r') as content_file:
+                    content = content_file.read()
+                    return content
+            except:
+                logging.error('Can\'t read /proc/meminfo')
+                return ''
+        else:
+            logging.error('Can\'t find /proc/meminfo')
+        return ''
+
+    def _parse_meminfo(self, data):
+        if data == '':
+            return {}
+        result = {}
+        for info in re.findall(r'^(\S+)\:\s+(\d+)\s+kB$', data, re.M):
+            result[info[0]] = int(info(1))*1024
+        result['_TOTAL'] = ''
+        if 'MemTotal' in result:
+            result['_TOTAL'] = self._humansize(result['MemTotal'])
+        result['_SWAP'] = ''
+        if 'SwapTotal' in result:
+            result['_TOTAL'] = self._humansize(result['SwapTotal'])
+        result['_CACHED'] = ''
+        if 'Cached' in result:
+            result['_CACHED'] = self._humansize(result['Cached'])
+        result['_DIRTY'] = ''
+        if 'Dirty' in result:
+            result['_DIRTY'] = self._humansize(result['Dirty'])
         return result
